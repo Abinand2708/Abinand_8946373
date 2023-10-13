880dcb51c03bf6ec56d1db5baa95e51a57f0a7d8
abinand <abinandreddie27@gmail.com>
2023-10-13 19:56:09 -0400

    code commited

--- Assignment 3/Assg3.py
+++ Assignment 3/Assg3.py
@@ -25,24 +25,24 @@ time.sleep(5)
 # Verifying that the search results page has loaded
 assert "Electronics: Amazon.ca" in driver.title
 
-# Selecting a course from the search results
+# Selecting smartwatches from the search results
 Smartwatches_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div[2]/div[2]/div[1]/div/div[1]/ul[1]/li[11]/a")
 # Course_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
 Smartwatches_link.click()
 
 
 
-# Waiting for the laptop details page to load
+# Waiting for the smartwatches details page to load
 time.sleep(5)
 
-# Adding the laptop to the cart
+# Adding the watch to the wishlist
 watch_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/div/div[2]/div[2]/ul/li[5]/span/div/a/div/div")
 watch_link.click()
 
-# Waiting for the cart to update
+# Waiting for the wishlist to update
 time.sleep(5)
 
-# Clicking on no thanks button
+# Clicking on wishlist button
 wishlist_link= driver.find_element("xpath","/html/body/div[2]/div/div[6]/div[4]/div[1]/div[3]/div/div[1]/div/div/div/form/div/div/div/div/div[7]/div[1]/div[1]/span/span/a")
 wishlist_link.click()
 time.sleep(2)
